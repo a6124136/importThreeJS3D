3de5ff6d503f8f5c854351f9de4c57396cbdf310
a6124136 <b6124136@gmail.com>
2025-03-03 20:57:09 +0800

    救命啊

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import * as THREE from './three.js/build/three.module.js';
-import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './node_modules/three/build/three.module.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 export default
 class App {
     constructor(){
